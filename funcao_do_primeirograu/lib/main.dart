32d1572a0cd6c902a8bad91a544110da43d57d3d
hp <igorferreiratavares95@gmail.com>
2022-04-09 14:23:34 -0300

    Testando ainda

--- funcao_do_primeirograu/lib/main.dart
+++ funcao_do_primeirograu/lib/main.dart
@@ -34,7 +34,7 @@ class TutorialHome extends StatelessWidget {
       ),
       // body is the majority of the screen.
       body: const Center(
-        child: Text('Sejam Bem Vindos'),
+        child: Text('Bem Vindos'),
       ),
       floatingActionButton: const FloatingActionButton(
         tooltip: 'Add', // used by assistive technologies
