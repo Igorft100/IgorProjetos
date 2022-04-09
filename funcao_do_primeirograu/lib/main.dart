a3f47f0813745d75f91ed38052600a79f7999dde
hp <igorferreiratavares95@gmail.com>
2022-04-09 14:30:51 -0300

    Função do 1ª Grau

--- funcao_do_primeirograu/lib/main.dart
+++ funcao_do_primeirograu/lib/main.dart
@@ -34,7 +34,7 @@ class TutorialHome extends StatelessWidget {
       ),
       // body is the majority of the screen.
       body: const Center(
-        child: Text('Bem Vindos'),
+        child: Text('Sejam Bem Vindos'),
       ),
       floatingActionButton: const FloatingActionButton(
         tooltip: 'Add', // used by assistive technologies
