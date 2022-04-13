f49dba49b383327e11996ecca52476f05b52a9f7
hp <igorferreiratavares95@gmail.com>
2022-04-13 19:52:51 -0300

    ATUALIZADO EM: 13/04/22, MAIS ÁRVORE E LOGO MARCA

--- funcao_do_primeirograu/lib/main.dart
+++ funcao_do_primeirograu/lib/main.dart
@@ -1,46 +1,17 @@
 import 'package:flutter/material.dart';
 
+import './step_form.dart';
+
 void main() {
-  runApp(
-    const MaterialApp(
-      title: 'Matemática Fácil',
-      home: TutorialHome(),
-    ),
-  );
+  runApp(Main());
 }
 
-class TutorialHome extends StatelessWidget {
-  const TutorialHome({Key? key}) : super(key: key);
-
+class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Scaffold is a layout for
-    // the major Material Components.
-    return Scaffold(
-      appBar: AppBar(
-        leading: const IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
-        ),
-        title: const Text('Função do 1ª Grau'),
-        actions: const [
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
-      ),
-      // body is the majority of the screen.
-      body: const Center(
-        child: Text('Sejam Bem Vindos'),
-      ),
-      floatingActionButton: const FloatingActionButton(
-        tooltip: 'Add', // used by assistive technologies
-        child: Icon(Icons.add),
-        onPressed: null,
-      ),
+    return MaterialApp(
+      title: 'Matemática Fácil',
+      home: StepForm(),
     );
   }
 }
---
+++ funcao_do_primeirograu/lib/step_form.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StepForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Função do 1ª Grau'),
+      ),
+      body: Text('Sejam Bem Vindos'),
+    );
+  }
+}
